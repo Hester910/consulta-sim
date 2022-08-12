81779faf5aab7b037e4acbeeeb1ba72c5724c89a
Hester910 <hadassa.ester@ufrpe.br>
2022-08-11 23:13:48 -0300

    Validations de Paciente

--- app/models/paciente.rb
+++ app/models/paciente.rb
@@ -3,4 +3,11 @@ class Paciente < ApplicationRecord
   accepts_nested_attributes_for :endereco, allow_destroy: true
   has_many :consultums, :dependent => :delete_all
   has_many :medicos, through: :consultums
+
+  validates :cpf, presence: {message: 'CPF obrigatório'},
+            length: {is: 11, message: 'Precisa ter 11 dígitos'},
+            format: {with: /\A\d+\z/, message: 'Deve ter apenas números'}
+  validates :nome, presence: {message: 'Nome é obrigatório'},
+            length: {maximum: 100, message:'Máximo 100 caracteres'}
+  validates :email, format: {with: URI:: MailTo::EMAIL_REGEXP}
 end
