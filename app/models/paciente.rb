fca1d592c5bc6ff6e01fffdef168b808d1885a04
Hester910 <hadassa.ester@ufrpe.br>
2022-08-11 22:51:53 -0300

    Revert "Associations de Paciente"
    This reverts commit 7ebec312d1e938b53915ed9d0ff94e83e1246a66.

--- app/models/paciente.rb
+++ app/models/paciente.rb
@@ -1,6 +1,2 @@
 class Paciente < ApplicationRecord
-  has_one :endereco, dependent: :destroy
-  accepts_nested_attributes_for :endereco, allow_destroy:true
-  has_many :consultums, :dependent => :delete_all
-  has_many :medicos, through: :consultums
 end
