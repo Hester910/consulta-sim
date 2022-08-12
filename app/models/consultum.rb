1eea17886a88fb4907471d1cb306562055c2bc2a
Hester910 <hadassa.ester@ufrpe.br>
2022-08-11 23:30:39 -0300

    Validations de Consulta

--- app/models/consultum.rb
+++ app/models/consultum.rb
@@ -1,4 +1,15 @@
 class Consultum < ApplicationRecord
   belongs_to :Paciente
   belongs_to :Medico
+
+  validates :data, :horario, presence: true
+  validate :validar_data
+  validates_associated :medico
+  validates_associated :paciente
+
+  def validar_data
+    if data.present? && data < Date.today
+      errors.add(:data, "A data deve ser de hoje ou futura")
+    end
+  end
 end
