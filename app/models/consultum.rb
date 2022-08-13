83e5fd17aba514763eb0929904aaad41cafc4d35
Hester910 <hadassa.ester@ufrpe.br>
2022-08-13 17:00:58 -0300

    Mudanca nos nomes das entidades

--- app/models/consultum.rb
+++ app/models/consultum.rb
@@ -4,8 +4,8 @@ class Consultum < ApplicationRecord
 
   validates :data, :horario, presence: true
   validate :validar_data
-  validates_associated :medico
-  validates_associated :paciente
+  validates_associated :Medico
+  validates_associated :Paciente
 
   def validar_data
     if data.present? && data < Date.today
