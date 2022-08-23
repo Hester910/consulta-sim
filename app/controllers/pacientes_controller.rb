fec5f92866b30a064f890acb7ea55e5889af27e8
Hester910 <hadassa.ester@ufrpe.br>
2022-08-23 16:12:19 -0300

    Desfazendo ultima alteracao

--- app/controllers/pacientes_controller.rb
+++ app/controllers/pacientes_controller.rb
@@ -64,7 +64,6 @@ class PacientesController < ApplicationController
   end
 
   private
-
     # Use callbacks to share common setup or constraints between actions.
     def set_paciente
       @paciente = Paciente.find(params[:id])
