82a9770ce56a32a31c335e0cbd654437129ce5c1
Hester910 <hadassa.ester@ufrpe.br>
2022-08-23 15:53:23 -0300

    Simples alteracao na associacao de Paciente com Consultas

--- app/controllers/pacientes_controller.rb
+++ app/controllers/pacientes_controller.rb
@@ -64,6 +64,7 @@ class PacientesController < ApplicationController
   end
 
   private
+
     # Use callbacks to share common setup or constraints between actions.
     def set_paciente
       @paciente = Paciente.find(params[:id])
