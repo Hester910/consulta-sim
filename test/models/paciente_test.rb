52c5f03f95419f1cb92f44f4238ac458ac2074ad
Hester910 <hadassa.ester@ufrpe.br>
2022-08-27 19:07:55 -0300

    Testes unitarios de Paciente

--- test/models/paciente_test.rb
+++ test/models/paciente_test.rb
@@ -4,4 +4,14 @@ class PacienteTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test 'salvar paciente com dados validos' do
+   paciente = Paciente.new nomeCompleto: 'Maria do Carmo', dataDeNascimento: DateTime.now,
+                           cpf: '12332145665', email: 'mariacarmo@gmail.com'
+   assert paciente.save
+  end
+
+  test 'salvar paciente vazio' do
+   paciente = Paciente.new
+   assert_not paciente.save
+  end
 end
